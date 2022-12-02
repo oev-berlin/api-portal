7dc52a82d68d59dd400f2715d3d3950e5ed9bef5
Mohammad Ahmad <mahmad@oev.de>
2022-12-02 16:51:12 +0100

    Code cleanup

--- components/Card/fragments/MicroService/Microservice.tsx
+++ components/Card/fragments/MicroService/Microservice.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, CardActionArea, Grid } from '@mui/material';
+import { CardActionArea, Grid } from '@mui/material';
 import { useRouter } from 'next/router';
 import { MicroserviceContainer } from './styles';
 import { MicroServiceProps } from './interfaces';
@@ -7,25 +7,17 @@ import { MicroServiceProps } from './interfaces';
 export function Microservice({ serviceName, isMicroservice }: MicroServiceProps) {
   const router = useRouter();
   function handelClick() {
-    router.push(`/project/${serviceName}`, undefined, { shallow: false }).then(() => console.log('redirected')).catch((e) => console.error(e));
+    if (isMicroservice) {
+      router.push(`/project/${serviceName}`);
+    }
   }
-  if (isMicroservice) {
-    return (
-      <Grid item xs={12} sm={4} md={6} lg={4}>
-        <CardActionArea component={Button} onClick={() => handelClick()}>
-          <MicroserviceContainer>
-            {serviceName}
-          </MicroserviceContainer>
-        </CardActionArea>
-      </Grid>
-    );
-  }
-
   return (
     <Grid item xs={12} sm={4} md={6} lg={4}>
-      <MicroserviceContainer elevation={1}>
-        {serviceName}
-      </MicroserviceContainer>
+      <CardActionArea onClick={() => handelClick()} disabled={!isMicroservice}>
+        <MicroserviceContainer>
+          {serviceName}
+        </MicroserviceContainer>
+      </CardActionArea>
     </Grid>
   );
 }
